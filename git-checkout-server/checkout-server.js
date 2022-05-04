ddb7e4e9a543e04461970ac36bcac5f2615a0253
shopnobaz <vision2025bd@yahoo.com>
2022-05-05 00:06:42 +0200

    added checkout function, dbpath and dbtemplate pat

--- git-checkout-server/checkout-server.js
+++ git-checkout-server/checkout-server.js
@@ -4,19 +4,23 @@ const http = require('http');
 //Use the bulit in execSync commando that can run
 // commnand line/bash commands
 const { execSync } = require('child_process');
+
 //Path helps build file paths
 const path = require('path');
+
+
 //Path to db-template
-const dbTemplatePath = path.join(__dirname, 'database', 'products-template.db');
-const dbPath = path.join(__dirname, 'database', 'products.db');
+const dbTemplatePath = path.join(__dirname, '../database', 'products-template.db');
+const dbPath = path.join(__dirname, '../database', 'products.db');
 
 
 // A function that does all neccessary git checkout, clean up etc
 function checkout() {
     //Run the git checkout command
     execSync('git pull'); // pull the latest changes from the remote repo
-    execSync('rm' + dbPath); // remove the old db file
-    execSync('cp' + dbTemplatePath + ' ' + dbPath); // copy the template db file to the new db file
+    //execSync('npm install'); // install new npm modules mentioned in package.json
+    execSync('rm ' + dbPath); // remove the old db file
+    execSync('cp ' + dbTemplatePath + ' ' + dbPath); // copy the template db file to the new db file
     //execSync('pm2 restart main-app');//restart our main-app
     console.log('Pulled, copied db and restarted the sever');
 }
